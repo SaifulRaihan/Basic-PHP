77eb6d7e9ba71f7e7365d328de0af579a616334c
Saiful <raihansaiful321@gmail.com>
2021-07-28 23:04:03 +0600

    Two file added 28-07-2021

---
+++ elseif.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $d = 'D';
+
+        if($d == 'Fri')
+            echo 'Have a nice weekend';
+        elseif($d == 'D')
+            echo 'Have a nice sunday';
+        else
+            echo 'Have a nice day';
+    ?>
+</body>
+</html>
